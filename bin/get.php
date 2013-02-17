3d90d69ce8d3d49e1368b6e5fc64adc8f48cbcf6
konfiot <arthurtoussaint@free.fr>
2013-02-17 11:42:46 +0100

    Recuperation des données de la BDD et envoi encodé en JSON

--- bin/get.php
+++ bin/get.php
@@ -1,3 +1,12 @@
 <?php
-echo file_get_contents("../data.tr");
-?>
+try {
+	$db = new PDO("sqlite:../data/db.sqlite");
+} catch (Exception $e) {
+	echo 'db:' . $e->getMessage();
+}
+
+$req = $db->prepare("SELECT no_capt, no_val, val, time FROM data");
+$req->execute(array());
+$data = $req->fetchAll(PDO::FETCH_NUM);
+echo json_encode($data);
+?> 
