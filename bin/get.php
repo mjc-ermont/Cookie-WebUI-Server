4f459fb0ac6adc88487885337a6b27981284b758
konfiot <arthurtoussaint@free.fr>
2013-02-24 11:30:53 +0100

    Changement du format du json, maintenant on acced aux capteurs comme ca data[id_capt][id_val] et ca renvoi un tableau de couples valeurs temps

--- bin/get.php
+++ bin/get.php
@@ -3,6 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("content-type: application/json");
 if (isset($_GET["t"])) {
+	$data = array();
 	$time = $_GET["t"];
 	try {
 		$db = new PDO("sqlite:../data/db.sqlite");
@@ -12,7 +13,14 @@ if (isset($_GET["t"])) {
 
 	$req = $db->prepare("SELECT no_capt, no_val, val, time FROM data WHERE time > ?");
 	$req->execute(array($time));
-	$data = $req->fetchAll(PDO::FETCH_NUM);
+	//$data = $req->fetchAll(PDO::FETCH_NUM);
+	while($rep = $req->fetch()){
+		if (!(isset($data[(int)$rep["no_capt"]][(int)$rep["no_val"]]))){
+			$data[(int)$rep["no_capt"]][(int)$rep["no_val"]] = array();
+		}
+		array_push($data[(int)$rep["no_capt"]][(int)$rep["no_val"]], array((int)$rep["val"], (int)$rep["time"]));
+	}
+	//var_dump($data);
 	if (count($data)) {
 		echo json_encode($data);
 	} else {
