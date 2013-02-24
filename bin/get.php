e65fc2d36e774588f81e288f66097f25492e0e3b
konfiot <arthurtoussaint@free.fr>
2013-02-24 13:28:53 +0100

    Maintenant; le temps est le temps par rapport au dernier envoi, ça économise des caractères

--- bin/get.php
+++ bin/get.php
@@ -1,9 +1,11 @@
 <?php
-
+ob_start();
 header("Access-Control-Allow-Origin: *");
 header("content-type: application/json");
 if (isset($_GET["t"])) {
+	//$first = TRUE;
 	$data = array();
+	$last = array();
 	$time = $_GET["t"];
 	try {
 		$db = new PDO("sqlite:../data/db.sqlite");
@@ -11,14 +13,19 @@ if (isset($_GET["t"])) {
 		echo 'db:' . $e->getMessage();
 	}
 
-	$req = $db->prepare("SELECT no_capt, no_val, val, time FROM data WHERE time > ?");
+	$req = $db->prepare("SELECT no_capt, no_val, val, time FROM data WHERE time > ? ORDER BY time");
 	$req->execute(array($time));
 	//$data = $req->fetchAll(PDO::FETCH_NUM);
 	while($rep = $req->fetch()){
+		/*if ($first){
+			$first = FALSE;
+			$reftime = (int)$rep["time"];
+		}*/
 		if (!(isset($data[(int)$rep["no_capt"]][(int)$rep["no_val"]]))){
 			$data[(int)$rep["no_capt"]][(int)$rep["no_val"]] = array();
 		}
-		array_push($data[(int)$rep["no_capt"]][(int)$rep["no_val"]], array((int)$rep["val"], (int)$rep["time"]));
+		array_push($data[(int)$rep["no_capt"]][(int)$rep["no_val"]], array((int)$rep["val"], (int)$rep["time"]-$last[(int)$rep["no_capt"]][(int)$rep["no_val"]]));
+		$last[(int)$rep["no_capt"]][(int)$rep["no_val"]] = (int)$rep["time"];
 	}
 	//var_dump($data);
 	if (count($data)) {
@@ -29,4 +36,6 @@ if (isset($_GET["t"])) {
 } else {
 	echo "p";
 }
+ob_end_flush();
+
 ?>
