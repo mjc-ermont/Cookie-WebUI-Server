35b9983c820bd850641d9ad972ef25efbd093bb6
Arthur <arthurtoussaint@free.fr>
2013-03-05 12:25:49 +0100

    Fix du path

--- bin/get_chrono.php
+++ bin/get_chrono.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("content-type: application/json");
 
-require '../constants.inc.php';
+require "../functions/cosntants.inc.php";
 
 if (file_exists(PATH_CHRONO)) {
 	file_get_contents(PATH_CHRONO);
